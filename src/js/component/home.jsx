1a4cfe8d3a00f7bcdadf8677a390335f12671583
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-09-18 13:59:21 +0000

    added control buttons

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react"
+import { render } from "react-dom";
+import ControlButtons from "./controlButtons";
 
 //create your first component
 const Home = () => {
+	const [time, setTime] = useState(0)
+	const [isActive, setIsActive] = useState(false);
+	const [isPaused, setIsPaused] = useState(true);
+
+
+
+	useEffect(() => {
+		let interval = null;
+
+		if (isActive && isPaused === false) {
+			interval = setInterval(() => {
+				setTime(time => time + 1);
+			}, 1000);
+		} else {
+			clearInterval(interval);
+		}
+		return () => {
+			clearInterval(interval);
+		};
+	}, [isActive, isPaused]);
+
+	const handleStart = () => {
+		setIsActive(true);
+		setIsPaused(false);
+	};
+
+	const handlePauseResume = () => {
+		setIsPaused(!isPaused);
+	};
+
+	const handleReset = () => {
+		setIsActive(false);
+		setTime(0);
+	};
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container d-flex">
+				<p>{(Math.floor(time / 1000)) % 10}</p>
+				<p>{(Math.floor(time / 100)) % 10}</p>
+				<p>{(Math.floor(time / 10)) % 10}</p>
+				<p>{(Math.floor(time / 1)) % 10}</p>
+			</div>
+			<div className="container">
+				<ControlButtons
+					active={isActive}
+					isPaused={isPaused}
+					handleStart={handleStart}
+					handlePauseResume={handlePauseResume}
+					handleReset={handleReset}
+				/>
+			</div>
+		</>
 	);
 };
 
